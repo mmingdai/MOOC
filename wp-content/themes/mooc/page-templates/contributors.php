347eb93676a578b5b14e9ed901f21876e2cd46b5
Mingming Dai <mmingdai@gmail.com>
2014-04-02 18:53:10 -0400

    contributors template

---
+++ wp-content/themes/mooc/page-templates/contributors.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template Name: Contributor Page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+
+get_header(); ?>
+
+<div id="main-content" class="main-content">
+
+	<textarea rows=5></textarea>
+	
+</div><!-- #main-content -->
+
+<?php
+get_sidebar();
+get_footer();
